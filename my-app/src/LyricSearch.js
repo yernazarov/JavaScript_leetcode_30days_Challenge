9b20d937608042b3d809ea5d0fd78fe6871eb5e9
zhandos <yernazarovz@gmail.com>
2023-10-08 21:52:31 +0600

    add hw4

--- my-app/src/LyricSearch.js
+++ my-app/src/LyricSearch.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import axios from 'axios';
 import LyricForm from './LyricForm';
 import LyricDisplay from './LyricDisplay';
@@ -8,11 +8,24 @@ function LyricSearch() {
   const [artist, setArtist] = useState('');
   const [song, setSong] = useState('');
 
-  useEffect(() => {
+  const apiUrl = useMemo(() => {
+    return `https://api.example.com/lyrics?artist=${artist}&song=${song}`;
   }, [artist, song]);
+  
 
-  const handleSearch = () => {
-  };
+  const handleSearch = useCallback(() => {
+    axios.get(apiUrl)
+      .then((response) => {
+        setLyrics(response.data.lyrics);
+      })
+      .catch((error) => {
+        console.error('Error fetching lyrics:', error);
+      });
+  }, [apiUrl]);
+
+  useEffect(() => {
+    handleSearch();
+  }, [artist, song, handleSearch]);
 
   return (
     <div>
